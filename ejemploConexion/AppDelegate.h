8bd9ab5fee9e82eee804ae0dac4ad87e936a29d6
reneciokaigen <rene.soto01@iest.edu.mx>
2015-09-19 19:36:39 -0500

    Initial Commit

---
+++ ejemploConexion/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ejemploConexion
+//
+//  Created by René Soto Lira on 19/09/15.
+//  Copyright (c) 2015 KAI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ejemploConexion/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ejemploConexion
+//
+//  Created by René Soto Lira on 19/09/15.
+//  Copyright (c) 2015 KAI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
